ad837042f27052ee44d20a031792eb73edd17f5e
Hamza Sahin <hsahin2@binghamton.edu>
2020-08-11 14:06:37 +0300

    Magaza Model Implemented

---
+++ Models/MagazaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TekzenApiClient.Models
+{
+    public class MagazaModel
+    {
+        public MekanResponseModel[] MekanResponse { get; set; }
+        public bool Basarili { get; set; }
+        public string Mesaj { get; set; }
+    }
+}
---
+++ Models/MekanResponseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TekzenApiClient.Models
+{
+    public class MekanResponseModel
+    {
+        public int MekanId { get; set; }
+        public string MekanKod { get; set; }
+        public string MekanAd { get; set; }
+        public string MekanTip { get; set; }
+        public string MekanAdres { get; set; }
+        public string MekanTel { get; set; }
+        public string MekanFaks { get; set; }
+        public short MekanKentAlanKod { get; set; }
+        public byte MekanKentPlaka { get; set; }
+        public int MekanIlceKod { get; set; }
+        public string MekanKent { get; set; }
+        public string MekanKentIlce { get; set; }
+    }
+}
